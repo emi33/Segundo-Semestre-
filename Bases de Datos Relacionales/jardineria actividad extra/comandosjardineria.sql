e1872b4adbb1496d85db7024eea399921ad66a76
emi33 <emiagustinchoque@gmail.com>
2024-06-05 12:10:46 -0300

    bases de datos

---
+++ Bases de Datos Relacionales/jardineria actividad extra/comandosjardineria.sql
@@ -0,0 +1,7 @@
+-- Devuelve un listado con el código de oficina y la ciudad donde hay oficinas.
+use jardineria;
+select codigo_oficina as codigo, ciudad as ciudad from oficina;
+
+
+
+-- cuenta firebase 12345,.-A!a
